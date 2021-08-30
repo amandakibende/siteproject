8ef9bdb71b2209945682443a1923451385967e50
Amanda KIBENDE <contactproamanda@gmail.com>
2021-08-30 14:40:14 +0200

    pages install

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Switch, Route } from 'react-router';
+import Home from "./pages/Home"; 
+import Temperature from './pages/Temperature';
+import Customize from './pages/Customize';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+   <nav>
+    <Switch>
+      <Route exact path="/" component={Home}></Route>
+      <Route exact path="/temperature" component={Temperature}></Route>
+      <Route exact path="/customize-image" component={Customize}></Route>
+    </Switch> 
+   </nav> 
    
     </div>
   );
---
+++ src/pages/Temperature.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Temperature() {
+    return (
+        <div>
+            <h1>Temperature!</h1>
+        </div>
+    )
+}
+
+export default Temperature
